96c460806e937023fcc431529918c7d59e89f473
Joseph Hornby <joehornby@gmail.com>
2020-10-20 22:12:43 +0100

    add 3d model and html markup to canvas

--- anamma/src/App.tsx
+++ anamma/src/App.tsx
@@ -1,31 +1,43 @@
 import React from 'react';
-import { Suspense } from 'react';
+import { useRef, Suspense } from 'react';
 import './App.scss';
 /* Components */
 import Header from "./components/header";
 /* React Three Fiber */
 import { Canvas } from "react-three-fiber";
-import { Html } from "drei";
+import { Html, Loader, useGLTFLoader } from "drei";
 
+const modelPath: string = '/forceps1.gltf';
+
+const Model = () => {
+  const gltf = useGLTFLoader(modelPath, true);
+  return <primitive object={gltf.scene} dispose={null} />;
+}
 
 function App() {
   return (
     <>
       <Header />
-      <Suspense fallback={null}>
-          <div className="container">
-            <div className="headline">
-              <h1>The only interview course you need.</h1>
-              <h2>Obstetrics and Gynaecology ST1 &amp; ST3 Interview Masterclasses.</h2>
-            </div>
-          </div>
-      </Suspense>
       <Canvas
         concurrent
         colorManagement
-        camera={{ position:[0, 0, 120], fov: 70 }}>
-          
-        </Canvas>
+        camera={{ position:[0, 0, 120], fov: 20 }}>
+        <Suspense fallback={null}>
+          <group position={[0, 0, 0]}>
+            <mesh position={[0, 0, 0]} scale={[1.2, 1.2, 1.2]} rotation={[0,10,10]}>
+              <Model />
+            </mesh>
+            <Html fullscreen>
+              <div className="container">
+                <div className="headline">
+                  <h1>The only interview course you need.</h1>
+                  <h2>Obstetrics and Gynaecology ST1 &amp; ST3 Interview Masterclasses.</h2>
+                </div>
+              </div>
+              </Html>
+          </group>
+        </Suspense>
+      </Canvas>
       
     </>
   );
